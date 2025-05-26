7b53ff80aaccc54c73b025ef3f29eca500b71aa4
mansoor <t.alyamani95@gmail.com>
2025-05-26 20:56:46 +0700

    Create pasien objects dynamically

--- Asosiasi.cpp
+++ Asosiasi.cpp
@@ -63,5 +63,8 @@ int main() {
     dokter* varDokter1 = new dokter("dr.Budi");
     dokter* varDokter2 = new dokter("dr.Tono");
 
-   
+    pasien* varPasien1 = new pasien("Andi");
+    pasien* varPasien2 = new pasien("Lia");
+
+    
 }
