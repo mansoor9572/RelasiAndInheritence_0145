0b3cc7e6413426d58e71bb1ae651b68ea4e27680
mansoor <t.alyamani95@gmail.com>
2025-05-26 18:58:35 +0700

    Create objects

--- agregasi/agregasi.cpp
+++ agregasi/agregasi.cpp
@@ -5,3 +5,14 @@ using namespace std;
 #include "Anak.h"
 #include "ibu.h"
  
+int main()
+{
+    ibu  *varIbu = new ibu ("dini");
+    ibu  *varIbu2 = new ibu ("novi");
+    anak *varAnak1 = new anak ("tono");
+    anak *varAnak2 = new anak ("rini");
+    anak *varAnak3 = new anak ("Dewi");
+
+  
+
+}
